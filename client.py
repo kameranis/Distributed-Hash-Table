b29976356f97c6a47f28a30e901d881617843bb8
gValkanos <el12162@central.ntua.gr>
2017-03-07 12:42:57 +0200

    server implementation 1st version

--- client.py
+++ client.py
@@ -1,14 +1,23 @@
-"""Handles all client side operations"""
+#!/usr/bin/env python
 
-class client(object):
-    """Client class"""
-    def init():
-        pass
+import socket
 
-    def query(key):
-        """Queries for a certain key and returns its value"""
-        raise NotImplementedError
+class Client:
 
-    def insert(key, value):
-        """Inserts a (key, value) pair to the DHT"""
-        raise NotImplementedError
+    def __init__(self,PORT):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(('localhost', PORT))        
+        
+    def make_query(self,question):
+
+        #data = raw_input ( "SEND :" )
+        self.client_socket.send(question)
+        answer=self.client_socket.recv(1024)
+        print answer
+
+
+x = Client(8881)
+y = raw_input("SEND: ")
+while y!='quit':         
+    x.make_query(y)
+    y = raw_input("SEND: ")
--- server.py
+++ server.py
@@ -1,43 +1,115 @@
-"""Server side code for the DHT
+#!/usr/bin/env python
 
-Contains the code executed for joining, inserting, quering and departing
-of the nodes."""
+import socket, select, Queue, re
+import sys
+from thread import *
 
-class server(object):
-    """Main server object"""
 
-    def __init__():
-        raise NotImplementedError
-
-    def join(sock):
-        """Performs all necessary actions for a node to join the DHT"""
-        raise NotImplementedError
-
-    def depart():
-        """Handles the graceful departure of the server"""
-        raise NotImplementedError
-
-    def insert(key, value):
-        """Inserts the (key, value) pair"""
-        raise NotImplementedError
-
-    def query(key):
-        """Returns the value of the associated key"""
-        raise NotImplementedError
-
-    def key_in_range(key):
-        """Returns True if the key belongs to this node"""
-        return this.previous_key < key <= this.key
-
-    def process_request(request):
-        request_type, request_payload = request
-        if request_type == 'query':
-            if key_in_range(request_payload[0]):
-                return query(request_payload[0])
+class server:
+        
+    def __init__(self,HOST,PORT,Prev_PORT,Next_PORT):
+        self.HOST=HOST
+        self.PORT=PORT
+        self.Next=-1
+        self.Prev=-1
+        self.client_socket_prev=socket.socket()
+        self.client_socket_next=socket.socket()
+        #open socket
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # bind socket at the given host,port
+        try:
+            self.s.bind((HOST, PORT))
+        except socket.error as msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit()
+        #sockets maximum listeners. We suppose they cant be more than 5
+        self.s.listen(5)
+        
+        self.connection_list=[self.s] #list of socket that send to server
+        self.write_to_client=[] #list of socket that server want to write
+        if Prev_PORT!=-1:
+            self.client_socket_prev = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket_prev.connect(('localhost', Prev_PORT))
+            self.client_socket_prev.send('next ' +  str(self.PORT))
+            self.client_socket_prev.recv(1024)
+            self.Prev=Prev_PORT
+        if Next_PORT!=-1:
+            if Next_PORT!=Prev_PORT:
+                self.client_socket_next = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client_socket_next.connect(('localhost', Next_PORT))
             else:
-                forward(request)
-        else if request_type == 'insert':
-            if key_in_range(request_payload[0]):
-                return insert(request_payload[0], request_payload[1])
-            else:
-                forward(request)
+                self.client_socket_next=self.client_socket_prev
+            
+            self.client_socket_next.send('prev ' + str(self.PORT))
+            self.client_socket_next.recv(1024)
+            self.Next=Next_PORT
+            
+        self.message_queues={} #servers' reply messages
+        
+    def accept_connection(self):
+        while 1:
+            read_sockets,write_sockets,error_sockets = select.select(self.connection_list,self.write_to_client,self.connection_list)
+            for sock in read_sockets:
+                if sock==self.s:
+                    #wait to accept a connection - blocking call
+                    conn, addr = self.s.accept()
+                    self.connection_list.append(conn)
+                    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+                    self.message_queues[conn] = Queue.Queue()
+                
+                else:
+                    data = sock.recv(1024)
+                    if data=='quit':
+                        if sock in self.write_to_client:
+                            self.write_to_client.remove(sock)
+                        self.connection_list.remove(sock)
+                        sock.close()
+                        del self.message_queues[sock]
+                    elif data:
+                        if data[:4]=='next':
+                            if self.Next!=-1:
+                                self.client_socket_next.close()
+                            self.Next = int(re.search(r'\d+', data).group())  
+                            self.client_socket_next = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            self.client_socket_next.connect(('localhost', self.Next))
+                            self.message_queues[sock].put('Connection granted...'+str(self.Next))
+                        elif data[:4]=='prev':
+                            if self.Prev !=-1:
+                                self.client_socket_prev.close()
+                            self.Prev = int(re.search(r'\d+', data).group())
+                            self.client_socket_prev = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            self.client_socket_prev.connect(('localhost', self.Prev))
+                            self.message_queues[sock].put('Connection granted...'+str(self.Prev))
+                        else:
+                            self.message_queues[sock].put('You send me... '+data)
+                        if sock not in self.write_to_client:
+                            self.write_to_client.append(sock)
+                                            
+            for sock in write_sockets:
+                try:
+                    next_msg = self.message_queues[sock].get_nowait()
+                except Queue.Empty:
+                    # No messages waiting so stop checking for writability.
+                    self.write_to_client.remove(sock)
+                else:
+                    sock.send(next_msg)
+
+            for sock in error_sockets:
+                print >>sys.stderr, 'handling exceptional condition for', sock.getpeername()
+                # Stop listening for input on the connection
+                self.connection_list.remove(sock)
+                if sock in self.write_to_client:
+                    self.write_to_client.remove(sock)
+                sock.close()
+                del self.message_queues[sock]
+        self.s.close()
+
+    def get_port(self):
+        return self.PORT
+
+    def get_host(self):
+        return self.HOST
+
+    def update(self):
+        return NotImplemented
+    
