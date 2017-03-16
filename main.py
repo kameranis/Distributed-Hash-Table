e4b3596bf0d946e94b273642cccb74655e2a67c1
kameranis <kameranis@gmail.com>
2017-03-16 17:39:53 +0200

    Add to main.py all the helping functions from test.py
    All of these need to be put in a class

--- main.py
+++ main.py
@@ -18,49 +18,10 @@ from multiprocessing import Process, Queue
 
 logging.basicConfig(filename='DHT.log', level=logging.DEBUG)
 
-processes = []
+processes = {}
 ports = {}
-
-
-def spawn_DHT(queue):
-    """Creates the first DHT server"""
-    master_server = Server_master('1')
-    queue.put(('1', master_server.get_port()))
-    master_server.accept_connection()
-
-
-def spawn_server(server_id, main_port, queue):
-    """Spawns a new DHT server and calls its join method"""
-    new_server = Server(server_id, main_port)
-    new_server.DHT_join()
-    queue.put((server_id, new_server.get_port()))
-    logging.debug('Server {} started'.format(server_id))
-    new_server.accept_connection()
-    sys.exit()
-
-
-def measure_throughput(filename):
-    """Reads requests from filename and forwards
-    each of them to a random server"""
-    start = time.time()
-    request_count = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            host = random.sample(ports, 1)[0]
-            port = ports[host]
-            with Client(port) as cli:
-                if line.startswith('query'):
-                    cli.make_query('query:{}'.format(*line.split(', ')[1:]))
-                elif len(line.split(', ')) == 1:
-                    cli.make_query('query:{}'.format(line))
-                elif line.startswith('insert'):
-                    cli.make_query('insert:-1:-1:{}:{}'.format(*line.split(', ')[1:]))
-                elif len(line.split(', ')) == 2:
-                    cli.make_query('insert:-1:-1:{}:{}'.format(*line.split(', ')))
-                else:
-                    raise ValueError('Bad Request')
-            request_count += 1
-    return start - time.time() / request_count
+queue = Queue()
+main_port = None
 
 
 def main():
@@ -75,33 +36,141 @@ def main():
 
     Closes the DHT"""
     # Create the DHT
-    queue = Queue()
-    processes.append(Process(target=spawn_DHT, args=(queue,)))
-    processes[0].start()
-    server_id, port = queue.get()
-    ports[server_id] = port
-    main_port = port
-    logging.debug('DHT created')
+    global main_port
+    processes['1'] = Process(target=create_DHT, args=(int(sys.argv[1]),))
+    processes['1'].start()
+    host, main_port = queue.get()
+    ports[host] = main_port
+    logging.debug('Server 1 started...')
     time.sleep(1)
-
-    # Add another 9 nodes
-    for i in xrange(2, 11):
-        processes.append(Process(target=spawn_server, args=(str(i), main_port, queue)))
-        processes[-1].start()
-        server_id, port = queue.get()
-        ports[server_id] = port
-        time.sleep(1)
+    for i in range(2, 10):
+        join(('join, ' + str(i)).split(', '))
+        # time.sleep(1)
 
     # Measure throughputs and report them to the user
     write_throughput = measure_throughput('insert.txt')
-    print 'Write throughput: ', write_throughput
+    print 'Write throughput:', write_throughput
     read_throughput = measure_throughput('query.txt')
-    print 'Read throughput: ', read_throughput
+    print 'Read throughput:', read_throughput
     request_throughput = measure_throughput('requests.txt')
-    print 'Request throughput: ', request_throughput
-    with Client(ports['1']) as cli:
-        cli.send_info('bye')
+    print 'Request throughput:', request_throughput
+    DHT_destroy([])
 
+
+def spawn_DHT(queue):
+    """Creates the first DHT server"""
+    master_server = Server_master('1', (sys.argv[1]))
+    queue.put(('1', master_server.get_port()))
+    master_server.accept_connection()
+
+
+def spawn_server(server_id):
+    """Creates a new server, has him join the DHT
+    and accept any incoming connections"""
+    server = Server(server_id, main_port)
+    server.DHT_join()
+    queue.put((server_id, server.get_port()))
+    logging.debug('Server ' + server_id + ' started...')
+    server.accept_connection()
+    sys.exit()
+
+
+def measure_throughput(filename):
+    """Reads requests from filename and forwards
+    each of them to a random server"""
+    start = time.time()
+    request_count = 0
+    with open(filename, 'r') as f:
+        for l in f:
+            line = l.rstrip()
+            if line.startswith('query'):
+                query(line.split(', '))
+            elif len(line.split(', ')) == 1:
+                query(['query', line])
+            elif line.startswith('insert'):
+                insert(line.split(', '))
+            elif len(line.split(', ')) == 2:
+                insert(['insert'] + line.split(', '))
+            else:
+                raise ValueError('Bad Request')
+            request_count += 1
+    return (time.time() - start) / request_count
+
+
+def join(command):
+    """Creates a new server in a new process.
+    Keeps the processes and ports updated"""
+    server_id = command[1]
+    processes[server_id] = Process(target=spawn_server, args=(server_id,))
+    processes[server_id].start()
+    host, port = queue.get()
+    ports[host] = port
+
+
+def depart(command):
+    """Commands the server to shut down"""
+    with Client(ports[command[1]]) as cli:
+        cli.communication(command[0])
+    processes[command[1]].join()
+    del processes[command[1]]
+    del ports[command[1]]
+
+
+def DHT_destroy(command):
+    """Forces the whole DHT to shutdown"""
+    with Client(ports['1']) as cli:
+        cli.communication('bye')
+
+
+def insert(command):
+    """Sends a request to a random server to insert a (key, value) pair"""
+    host = random.sample(ports, 1)[0]
+    port = ports[host]
+    with Client(port) as cli:
+        cli.communication('{}:{}:{}'.format(*command))
+
+
+def delete(command):
+    """Sends a request to a random server to delete the key"""
+    host = random.sample(ports, 1)[0]
+    port = ports[host]
+    with Client(port) as cli:
+        cli.communication('{}:{}'.format(*command))
+
+
+def query(command):
+    """Queries a random server for the value of a key"""
+    host = random.sample(ports, 1)[0]
+    port = ports[host]
+    if command[1] == '*':
+        with Client(port) as cli:
+            cli.communication('print_all_data')
+    else:
+        with Client(port) as cli:
+            cli.communication('{}:-1:-1:{}'.format(*command))
+
+
+def DHT_print(command):
+    """Requests the DHT topology from the master server"""
+    with Client(ports['1']) as cli:
+        print cli.communication(command[0])
+
+
+def create_DHT(repl):
+    """Creates the master server of the DHT"""
+    k = Server_master('1', repl)
+    queue.put(('1', k.get_port()))
+    k.accept_connection()
+    sys.exit()
+
+
+command_dict = {'join': join,
+                'depart': depart,
+                'exit': DHT_destroy,
+                'insert': insert,
+                'delete': delete,
+                'query': query,
+                'print': DHT_print}
 
 if __name__ == '__main__':
     main()
